42a5b1361848b055cb5597cb698a639f5a65dd93
shrvn12 <singhshravan1208@gmail.com>
2025-04-02 20:52:36 +0530

    return added

--- index.js
+++ index.js
@@ -7,19 +7,17 @@ const app = express();
 app.use(cors()); // Enable CORS for your frontend
 
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    return res.send("Hello World");
 })
 
 app.get('/jiosaavn-data', async (req, res) => {
     try {
         const jiosaavnResponse = await axios.get('https://www.jiosaavn.com/api.php?__call=webapi.getLaunchData&api_version=4&_format=json&_marker=0&ctx=web6dot0');
-        res.json(jiosaavnResponse.data);
+        return res.json(jiosaavnResponse.data);
     } catch (error) {
         console.error('Error fetching from JioSaavn:', error);
-        res.status(500).json({ error: 'Failed to fetch data from JioSaavn' });
+        return res.status(500).json({ error: 'Failed to fetch data from JioSaavn' });
     }
 });
 
-module.exports = (req, res) => {
-    app(req, res);
-};
+module.exports = app
